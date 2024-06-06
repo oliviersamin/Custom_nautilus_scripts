382c6b07e9e49d75a4032bb66798b69179845a97
Olivier <oliviersamin@gmail.com>
2024-06-06 18:57:25 +0200

    script to concatenate txt files OK. Need to be integrated into general script

--- python_scripts/Audio/concatenate_text_files.py
+++ python_scripts/Audio/concatenate_text_files.py
@@ -1,7 +1,18 @@
 import os
 
 
-def concatenate_files(input_files, output_file):
+def _set_output_file_name(input_files):
+    """
+    the hypothesis here is that the list of text files always contains as first value the exact name
+    of the audio translated without added values. So this one will be used
+    :param input_files:
+    :return: output_file
+    """
+    return input_files[0][:input_files[0].find(".")] + "_all.txt"
+
+def concatenate_files(input_files):
+    output_file = _set_output_file_name(input_files)
+    print(output_file)
     with open(output_file, 'w') as outfile:
         for fname in input_files:
             if os.path.isfile(fname):
@@ -16,9 +27,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Concatenate several text files into one.')
-    parser.add_argument('input_files', metavar='N', type=str, nargs='+', help='List of input text files to concatenate')
-    parser.add_argument('output_file', type=str, help='Output file name')
-
+    parser.add_argument('-f', '--input_files', type=str, help='List of input text files to concatenate')
     args = parser.parse_args()
-
-    concatenate_files(args.input_files, args.output_file)
+    input_files = args.input_files.split(",")
+    concatenate_files(input_files)
